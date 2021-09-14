1e2511fa77fc5347f5e256c70388821b943f4d84
jangjh0201 <jangjh0201@gmail.com>
2021-09-15 04:07:15 +0900

    2nd commit

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -46,6 +46,8 @@ import org.springframework.samples.petclinic.model.Person;
 @Table(name = "owners")
 public class Owner extends Person {
 
+	private Integer age;
+
 	@Column(name = "address")
 	@NotEmpty
 	private String address;
@@ -61,6 +63,14 @@ public class Owner extends Person {
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
 	private Set<Pet> pets;
+
+	public Integer getAge() {
+		return this.age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
 	public String getAddress() {
 		return this.address;
@@ -143,7 +153,7 @@ public class Owner extends Person {
 		return new ToStringCreator(this)
 
 				.append("id", this.getId()).append("new", this.isNew()).append("lastName", this.getLastName())
-				.append("firstName", this.getFirstName()).append("address", this.address).append("city", this.city)
+				.append("firstName", this.getFirstName()).append("age", this.age).append("address", this.address).append("city", this.city)
 				.append("telephone", this.telephone).toString();
 	}
 
